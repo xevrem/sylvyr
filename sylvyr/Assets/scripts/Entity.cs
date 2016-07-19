ce002b03875dcdb2a2303fd4edd99910e3ed4c71
erika jonell <erika.jonell@gmail.com>
2016-07-19 11:04:46 -0400

    starting base entity work

---
+++ sylvyr/Assets/scripts/Entity.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Entity : MonoBehaviour {
+
+	public string entity_name = "base entity";
+	public int hp;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
